ea0ac9f8f0ce8d08628494023a8c111a70c33025
Artur <qwert@gmail.com>
2024-11-13 00:28:35 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,22 @@
+// const res=confirm("Are you here?");
+// console.log(res);
+
+// const answer = +prompt("are you 18?","");
+// console.log(answer+5);
+//  const answers = [];
+// answers[0] = prompt("are you 18?","");
+// answers[1] = prompt("what's your name?","");
+// answers[2] = prompt("do you like our site?","");
+
+// // console.log(typeof(answers));
+// const category = 'toys';
+// console.log(`http://someurl.com/${category}/5`);
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+console.log(++incr);
+console.log(--decr);
+
+console.log(2*4 === "8");
